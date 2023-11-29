0cd6b4c98921abf53e45447d9fa1ac13f68ad5fb
bwook00 <bwook00@naver.com>
2023-11-29 05:46:05 +0000

    reuse pypdf in pdf_utils.py use tempfile in app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
+import os
 from http import HTTPStatus
 from typing import List, Dict, Union, Any
 
 from fastapi.middleware.cors import CORSMiddleware
+import tempfile
 
 import deepdoctection as dd
 from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI(title="DeepDoctection API")
-origins = []
+origins = ["http://localhost", "http://127.0.0.1"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -30,26 +32,32 @@ def root():
 @app.post("/extract")
 async def extract(file: UploadFile = File(...)) -> List[Dict[str, Union[Union[int, List[str], List[Any]], Any]]]:
     analyzer = dd.get_dd_analyzer()
-    df = analyzer.analyze(path=file.file)
+    # Save the uploaded file to a temporary location
+    with tempfile.NamedTemporaryFile(delete=True) as tmp:
+        tmp.write(await file.read())
+        
+    # Get the file path
+    file_path = tmp.name
+
+    # Analyze the file
+    df = analyzer.analyze(path=file_path)
+    
     df.reset_state()
     doc = iter(df)
 
     pages_result = []
     for i, page in enumerate(doc):
-        content = []
+        title = []
         for layout in page.layouts:
             if layout.category_name == "title":
-                content.append(f'title : {layout.text}')
+                title.append(f"{layout.text}")
                 # title = layout.text
-            elif layout.category_name == "list":
-                content.append(f'list : {layout.text}')
-                # list = layout.text
         text = page.text
         tables = page.tables or []
         tables_html = [table.html for table in tables]
         page_result = {
             "page_number": i + 1,
-            "content": content,
+            "title": title,
             "text": text,
             "table": tables_html,
         }
@@ -60,7 +68,7 @@ async def extract(file: UploadFile = File(...)) -> List[Dict[str, Union[Union[in
 def main():
     import uvicorn
 
-    uvicorn.run("app:app", port=8000)
+    uvicorn.run("app:app", host="0.0.0.0" ,port=8000)
 
 
 if __name__ == "__main__":
